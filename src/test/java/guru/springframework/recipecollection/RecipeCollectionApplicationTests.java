ccf06026463b3d54d7728afeba30e02f4e7e2162
frank zhang <yfrankzhang@yahoo.com>
2020-12-16 21:46:40 -0500

    initiate project add

---
+++ src/test/java/guru/springframework/recipecollection/RecipeCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.recipecollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeCollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
