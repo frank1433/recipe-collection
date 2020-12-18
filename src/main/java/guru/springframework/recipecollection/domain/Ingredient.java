5a5eb02a32601b82c353da9f48a8555b60baad67
frank zhang <yfrankzhang@yahoo.com>
2020-12-18 11:12:15 -0500

    recipe service implemented.

--- src/main/java/guru/springframework/recipecollection/domain/Ingredient.java
+++ src/main/java/guru/springframework/recipecollection/domain/Ingredient.java
@@ -20,7 +20,13 @@ public class Ingredient {
     @OneToOne(fetch =FetchType.EAGER)
     private UnitOfMeasure uom;
 
-    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom,Recipe recipe) {
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+    }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
         this.description = description;
         this.amount = amount;
         this.recipe = recipe;
